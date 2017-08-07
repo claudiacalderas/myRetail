bd617bf2e03d977f35fd1fa65584caebdaa98993
Claudia Calderas <claudia.calderas@gmail.com>
2017-08-06 21:37:07 -0500

    carousel logic working

--- client/scripts/controllers/productController.js
+++ client/scripts/controllers/productController.js
@@ -8,6 +8,7 @@ myRetailApp.controller('ProductController', ['$scope','$http', 'DataService',
   // variables to show/noshow addToCart and pickUp buttons
   $scope.pickUpVisible = false;
   $scope.addToCartVisible = false;
+  var numOfAlternateImages;
 
   // Calls Factory function that gets catalog information from the database
   DataService.getCatalogItem().then(function(data){
@@ -47,36 +48,44 @@ myRetailApp.controller('ProductController', ['$scope','$http', 'DataService',
     for (var i = 0; i < $scope.catalogItem.Images[0].AlternateImages.length; i++) {
       var imageObject = {};
       imageObject.image = $scope.catalogItem.Images[0].AlternateImages[i].image;
-      if (i < NUM_VISIBLE_IMAGES) {
-        imageObject.i = i;
-        imageObject.visible = true;
-      } else {
-        imageObject.i = i;
-        imageObject.visible = false;
-      }
       $scope.slides.push(imageObject);
     }
+    numOfAlternateImages = $scope.slides.length;
   }
 
   // slice index
   var slideIndex = 0;
+  $scope.arrayOfVisibleSlides = [0,1,2];
 
   // moves slides back or forth depending on parameter n (+1, -1)
   $scope.moveSlides = function(n) {
     if (n == 1) {
-      $scope.slides[slideIndex].visible = false;
-
-      if ((slideIndex + NUM_VISIBLE_IMAGES + n) >= $scope.slides.length) {
-        slideIndex = 0;
-      } else if ((slideIndex + n) <= 0) {
-        slideIndex = $scope.slides.length - 1;
+      // evaluates if last image displayed is the last one and in that case
+      // circles to the first image of the array
+      console.log('comparando:',$scope.arrayOfVisibleSlides[NUM_VISIBLE_IMAGES - 1], (numOfAlternateImages - 1));
+      if ($scope.arrayOfVisibleSlides[NUM_VISIBLE_IMAGES - 1] == (numOfAlternateImages - 1)) {
+        $scope.arrayOfVisibleSlides.shift();
+        $scope.arrayOfVisibleSlides.push(0);
+        console.log('1: arreglo es:', $scope.arrayOfVisibleSlides);
       } else {
-        slideIndex += n;
+        $scope.arrayOfVisibleSlides.shift();
+        $scope.arrayOfVisibleSlides.push($scope.arrayOfVisibleSlides[$scope.arrayOfVisibleSlides.length - 1] + 1);
+        console.log('2: arreglo es:', $scope.arrayOfVisibleSlides);
       }
-
-      $scope.slides[slideIndex + NUM_VISIBLE_IMAGES].visible = true;
+    } else {
+      // evaluates if first image displayed is the last one and in that case
+      // circles to the last image of the array
+      console.log('comparando:',$scope.arrayOfVisibleSlides[NUM_VISIBLE_IMAGES - 1], (numOfAlternateImages - 1));
+      if ($scope.arrayOfVisibleSlides[0] == 0) {
+        $scope.arrayOfVisibleSlides.pop();
+        $scope.arrayOfVisibleSlides.unshift($scope.slides.length-1);
+        console.log('1: arreglo es:', $scope.arrayOfVisibleSlides);
+      } else {
+        $scope.arrayOfVisibleSlides.pop();
+        $scope.arrayOfVisibleSlides.unshift($scope.arrayOfVisibleSlides[0] - 1);
+        console.log('2: arreglo es:', $scope.arrayOfVisibleSlides);
+      }
     }
-
   }
 
 }]);
